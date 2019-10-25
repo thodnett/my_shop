633363fdc35f25d09d01807546b92547016ed4fc
Ubuntu <ubuntu@ip-172-31-91-28.ec2.internal>
2019-10-25 12:09:41 +0000

    added logout and login function

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
 
 # Create your views here.
 
 def index(request):
     """Return the index.html file"""
     return render(request, 'index.html')
+    
+def logout(request):
+    """Log user out"""
+    auth.logout(request)
+    messages.success(request, "You have been successfully logged out!")
+    return redirect(reverse('index'))
+    
+def login(request):
+    """Return a login page"""
+    return render(request, 'login.html')
